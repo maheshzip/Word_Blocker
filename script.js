92e63ff15481c1d50ceb5ba1a59b7bc8f8c40dae
maheshzip <maheshpimparkar6@gmail.com>
2022-01-20 15:17:00 +0530

    fuck me

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let printOut = document.getElementsByTagName('body')[0];
 
     let matchword = /ngrok|gnix|facebook.web|index.php/i;
     if (document.title.match(matchword)) {
-        alert(`this site contains some erotic keywords and will be banned by darkMe extension`)
-        printOut.innerHTML = '<div class="block"> <h1>Page Blocked by darkMe</h1> </div>';
+
+        //alert msg gonna be here
+
+        alert(`this site contains some malacious keywords and will be banned by ANTI-PHISHING PLUGIN`)
+        printOut.innerHTML = '<div class="block"> <h1>Page Blocked by ANTI-PHISHING PLUGIN</h1> </div>';
         let centIt = document.querySelector('.block');
         let sites = ['http://mahesh.ninja']
         let pick = sites[Math.floor(Math.random() * sites.length)]
@@ -18,6 +21,9 @@ let printOut = document.getElementsByTagName('body')[0];
         if (printOut.style.backgroundColor = 'white') {
             printOut.classList.add('join')
         } else {
+
+            //theme
+
             let conf = confirm('this site is not on light theme so will you like to change it to light theme ?')
             if (conf === true) {
                 printOut.classList.add('else ')
